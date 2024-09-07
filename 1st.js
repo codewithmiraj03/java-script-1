565a39bf8c294a7fb7a0761f74b24eec1e1fa4dc
codewithmiraj03 <mirajahmed0314@gmail.com>
2024-09-08 00:44:00 +0500

    first java

---
+++ 1st.js
@@ -0,0 +1,42 @@
+    //    Alert
+
+
+
+
+alert("Error! please enter a valid password.")
+
+alert("Wellcom to JS land...\nHappy Coding!")
+
+console.log(alert)
+
+
+        //String Variabels
+
+        
+
+var name = "My Name is: Miraj";
+var age = "My Age is: 20";
+var student = "I am a Student of: Web development";
+
+alert(name);
+alert(age);
+alert(student);
+
+
+var pizza = "pizza\npizz\npiz\npi\np";
+
+alert(pizza);
+console.log(name, age, student, pizza);
+
+
+                // number variables
+
+var one = "Iam"
+var age = 20;
+var sec = "YearOld"
+
+alert(one+age+sec)
+
+
+console.log(one,age,sec)
+
